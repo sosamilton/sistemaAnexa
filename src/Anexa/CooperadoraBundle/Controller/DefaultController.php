43bf8a8f0b272e101b6a4e658d469afe4109f784
Milton Sosa <milton.sosa.22@gmail.com>
2019-02-25 16:21:08 -0300

    mail

--- src/Anexa/CooperadoraBundle/Controller/DefaultController.php
+++ src/Anexa/CooperadoraBundle/Controller/DefaultController.php
@@ -10,4 +10,12 @@ class BackendController extends Controller
     {
         return $this->render('AnexaCooperadoraBundle:backend:index.html.twig', array('menu' => 'inicio' ));
     }
+
+    public function sendMailAction(){
+      $to = "milton.sosa.22@gmail.com";
+      $subject = "Cooperadora de Escuela Anexa";
+      $txt = "Hello world!";
+      $headers = "From: milton.sosa@info.unlp.edu.ar" . "\r\n";
+      mail($to,$subject,$txt,$headers);
+    }
 }
